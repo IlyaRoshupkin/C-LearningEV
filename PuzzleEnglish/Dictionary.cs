9f0ee99c91fc1b33590863afd38e265d86b24292
Илья Рощупкин <roshupkin.ilya2012@yandex.ru>
2020-06-18 16:58:21 +0300

    Outlook and Init()

---
+++ PuzzleEnglish/Dictionary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleEnglish
+{
+    class Dictionary
+    {
+
+        int total = 0;
+        string[] english;
+        string[] russian;
+        string[] audioRus;
+        string[] audioEng;
+        string[] image;
+        string[] video;
+
+        public Dictionary()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            total = 0;
+            DirectoryInfo directoryInfo = new DirectoryInfo("data\\text\\");
+            FileInfo[] info = directoryInfo.GetFiles("*.txt", SearchOption.TopDirectoryOnly);
+            total = info.Length;
+
+            english = new string[total];
+            russian = new string[total];
+            audioRus = new string[total];
+            audioEng = new string[total];
+            image = new string[total];
+            video = new string[total];
+
+            foreach(FileInfo file in info)
+            {
+                string fileName = file.FullName;
+                string name = file.Name.Replace(".txt", "");
+            }
+
+        }
+    }
+}
---
+++ PuzzleEnglish/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace PuzzleEnglish
+{
+    partial class picCard
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrev = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.lblRussian = new System.Windows.Forms.Label();
+            this.lblEnglish = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblVideo = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPrev
+            // 
+            this.btnPrev.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrev.Font = new System.Drawing.Font("Verdana", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnPrev.Location = new System.Drawing.Point(111, 190);
+            this.btnPrev.Name = "btnPrev";
+            this.btnPrev.Size = new System.Drawing.Size(105, 80);
+            this.btnPrev.TabIndex = 0;
+            this.btnPrev.Text = "<<";
+            this.btnPrev.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNext.Font = new System.Drawing.Font("Verdana", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnNext.Location = new System.Drawing.Point(544, 190);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(105, 80);
+            this.btnNext.TabIndex = 1;
+            this.btnNext.Text = ">>";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // lblRussian
+            // 
+            this.lblRussian.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblRussian.Font = new System.Drawing.Font("Verdana", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblRussian.ForeColor = System.Drawing.Color.OrangeRed;
+            this.lblRussian.Location = new System.Drawing.Point(222, 39);
+            this.lblRussian.Name = "lblRussian";
+            this.lblRussian.Size = new System.Drawing.Size(316, 100);
+            this.lblRussian.TabIndex = 2;
+            this.lblRussian.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblEnglish
+            // 
+            this.lblEnglish.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblEnglish.Font = new System.Drawing.Font("Verdana", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblEnglish.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.lblEnglish.Location = new System.Drawing.Point(222, 326);
+            this.lblEnglish.Name = "lblEnglish";
+            this.lblEnglish.Size = new System.Drawing.Size(316, 100);
+            this.lblEnglish.TabIndex = 3;
+            this.lblEnglish.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(222, 142);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(316, 181);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblVideo
+            // 
+            this.lblVideo.AutoSize = true;
+            this.lblVideo.Font = new System.Drawing.Font("Verdana", 7.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblVideo.Location = new System.Drawing.Point(585, 87);
+            this.lblVideo.Name = "lblVideo";
+            this.lblVideo.Size = new System.Drawing.Size(67, 17);
+            this.lblVideo.TabIndex = 5;
+            this.lblVideo.Text = "+ video";
+            this.lblVideo.Visible = false;
+            // 
+            // picCard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 476);
+            this.Controls.Add(this.lblVideo);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblEnglish);
+            this.Controls.Add(this.lblRussian);
+            this.Controls.Add(this.btnNext);
+            this.Controls.Add(this.btnPrev);
+            this.Name = "picCard";
+            this.Text = "Puzzle English";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrev;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Label lblRussian;
+        private System.Windows.Forms.Label lblEnglish;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblVideo;
+    }
+}
+
